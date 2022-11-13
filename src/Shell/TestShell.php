b4f8997fe7efb473e8f53214f3bfbe3a5d4e9355
HoangVen <nguyenhoangven97@gamil.com>
2022-11-13 15:42:32 +0700

    Add scource

---
+++ src/Shell/TestShell.php
@@ -0,0 +1,13 @@
+<?php
+namespace App\Shell;
+use Cake\Console\Shell;
+use Cake\Log\Log;
+class TestShell extends Shell 
+{
+    public function runProcess()
+    {
+        Log::debug(111111111111);
+        Log::info('this gets written only to shops.log', ['scope' => ['textlog']]);
+        dd('Hello');
+    }
+}
